baa04b0641a9f233c6b9d420dd71e32c922d1670
francesco-abritta <fra.abritta@gmail.com>
2022-03-04 11:08:38 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $array_comics = config('comics');
+    //ci dovra essere un fumetti.blade.php
+    return view('fumetti',['comics'=>$array_comics]);
+});
